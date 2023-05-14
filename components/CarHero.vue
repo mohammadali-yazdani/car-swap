f4cb26936ead675347194ed645db2c93aebcdc43
Mohammad Ali <aliyazdanim2@gmail.com>
2023-05-14 21:55:54 +0330

    refactor: separate car search bar component

--- components/CarHero.vue
+++ components/CarHero.vue
@@ -6,16 +6,7 @@
       <div class="text-center w-1/2">
         <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
         <!-- HOME SEARCH BAR -->
-        <div
-          class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
-        >
-          <input
-            type="text"
-            class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-            placeholder="Search by city..."
-          />
-          <button class="bg-sky-500 px-10 text-white">Search</button>
-        </div>
+        <car-search-bar />
         <!-- HOME SEARCH BAR -->
       </div>
     </div>
